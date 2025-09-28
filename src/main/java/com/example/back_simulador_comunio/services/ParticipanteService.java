8937fe60f5d49d7129916badd5b4c875c2acfe59
Jorge <skmrubik@gmail.com>
2025-09-28 13:39:40 +0200

    Eliminación de kafka

--- src/main/java/com/example/back_simulador_comunio/services/ParticipanteService.java
+++ src/main/java/com/example/back_simulador_comunio/services/ParticipanteService.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
